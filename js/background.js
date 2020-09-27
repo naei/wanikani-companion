8c145fa2cb271ac95b30d3cb947cb5b8a21583f1
nashsibanda <contact@nashsibanda.com>
2020-09-27 16:39:25 +0100

    Switch to v2 API, add email addr. for gravatar

--- js/background.js
+++ js/background.js
@@ -1,16 +1,14 @@
-window.onload = function() {
-  
+window.onload = function () {
   var loopRequestId;
-
   // initialize the badge color
-  chrome.browserAction.setBadgeBackgroundColor({color:'#ff00aa'}); 
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#ff00aa" });
 
   // check that there is a Chrome sync value
   chrome.storage.sync.get("wkUserData", function (obj) {
-    if (obj.wkUserData === undefined){
-      if (localStorage.wkUserData === undefined){
-          // if a local storage does not exist, initialize it
-          setWkUserData(new WkUserData());
+    if (obj.wkUserData === undefined) {
+      if (localStorage.wkUserData === undefined) {
+        // if a local storage does not exist, initialize it
+        setWkUserData(new WkUserData());
       }
     } else {
       // get the existing user data from Chrome sync
@@ -18,18 +16,23 @@ window.onload = function() {
     }
     loopRequestUserData();
   });
-  
+
   // update data every x milliseconds
-  function loopRequestUserData(){
+  function loopRequestUserData() {
     var wkUserData = JSON.parse(localStorage.wkUserData);
-    requestUserData(true, function(){
-      loopRequestId = window.setTimeout(loopRequestUserData, wkUserData.refreshInterval, true, true);
+    requestUserData(true, function () {
+      loopRequestId = window.setTimeout(
+        loopRequestUserData,
+        wkUserData.refreshInterval,
+        true,
+        true
+      );
     });
   }
 
   // when the update interval is changed, restart loopRequestUserData with the updated interval
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
-    if ('wkUserData' in changes && loopRequestId !== undefined) {
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if ("wkUserData" in changes && loopRequestId !== undefined) {
       var oldValues = changes.wkUserData.oldValue;
       var newValues = changes.wkUserData.newValue;
       if (newValues.refreshInterval != oldValues.refreshInterval) {
@@ -41,19 +44,19 @@ window.onload = function() {
 
   // disable 'X-Frame-Options' header to allow inlining pages within an iframe
   chrome.webRequest.onHeadersReceived.addListener(
-      function(details) {
-          var headers = details.responseHeaders;
-          for (var i = 0; i < headers.length; ++i) { 
-            if (headers[i].name == 'X-Frame-Options') {
-                headers.splice(i, 1);
-                break;
-            }
-          }
-          return {responseHeaders: headers};
-      },
-      {
-          urls: [ '*://*/*' ]
-      },
-      ['blocking', 'responseHeaders']
+    function (details) {
+      var headers = details.responseHeaders;
+      for (var i = 0; i < headers.length; ++i) {
+        if (headers[i].name == "X-Frame-Options") {
+          headers.splice(i, 1);
+          break;
+        }
+      }
+      return { responseHeaders: headers };
+    },
+    {
+      urls: ["*://*/*"],
+    },
+    ["blocking", "responseHeaders"]
   );
-}
+};
--- js/home.js
+++ js/home.js
@@ -1,14 +1,16 @@
-window.onload = function() {
-
+window.onload = function () {
   var wkUserData = JSON.parse(localStorage.wkUserData);
   fullfillUserData();
 
   // display info message if the user is coming for the first time
-  if (wkUserData.userPublicKey === undefined || wkUserData.userPublicKey == ""){
-    document.querySelector(".info").style.display = 'inline';
+  if (
+    wkUserData.userPublicKey === undefined ||
+    wkUserData.userPublicKey == ""
+  ) {
+    document.querySelector(".info").style.display = "inline";
   } else {
-     // reload user data
-    requestUserData(false, function() {
+    // reload user data
+    requestUserData(false, function () {
       wkUserData = JSON.parse(localStorage.wkUserData);
       fullfillUserData();
     });
@@ -16,66 +18,71 @@ window.onload = function() {
 
   // display Gravatar image (if exist)
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", 'http://www.gravatar.com/avatar/' + wkUserData.gravatar + '?d=404', true);
-  xhr.onreadystatechange = function() {
+  xhr.open(
+    "GET",
+    "http://www.gravatar.com/avatar/" + wkUserData.gravatar + "?d=404",
+    true
+  );
+  xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
-        document.getElementById('gravatar').src = 'http://www.gravatar.com/avatar/' + wkUserData.gravatar;
+        document.getElementById("gravatar").src =
+          "http://www.gravatar.com/avatar/" + wkUserData.gravatar;
       }
     }
-  }
+  };
   xhr.send();
 
   // when the user click on a link, it redirect the url to the web-container page or a new Chrome tab (depends on user settings)
   var inApp = wkUserData.inAppNavigation;
-  document.getElementById('toLessons').onclick = function() {
-    var url = "https://www.wanikani.com/lesson/session"
-    if (inApp){
+  document.getElementById("toLessons").onclick = function () {
+    var url = "https://www.wanikani.com/lesson/session";
+    if (inApp) {
       localStorage.toLink = url;
     } else {
       chrome.tabs.create({ url: url });
     }
-  }
-  document.getElementById('toReviews').onclick = function() {
+  };
+  document.getElementById("toReviews").onclick = function () {
     var url = "https://www.wanikani.com/review/session";
-    if (inApp){
-     localStorage.toLink = url;
+    if (inApp) {
+      localStorage.toLink = url;
     } else {
-     chrome.tabs.create({ url: url });
+      chrome.tabs.create({ url: url });
     }
-  }
-  document.getElementById('toDashboard').onclick = function() {
-     var url = "https://www.wanikani.com/login";
-     if (inApp){
-       localStorage.toLink = url;
-     } else {
-       chrome.tabs.create({ url: url });
-     }
-  }
+  };
+  document.getElementById("toDashboard").onclick = function () {
+    var url = "https://www.wanikani.com/login";
+    if (inApp) {
+      localStorage.toLink = url;
+    } else {
+      chrome.tabs.create({ url: url });
+    }
+  };
 
   // fullfill user data
-  function fullfillUserData(){
+  function fullfillUserData() {
+    document.getElementById("username").innerHTML = wkUserData.username;
+    document.getElementById("level").innerHTML = wkUserData.level;
+    document.getElementById("nbLessons").innerHTML = wkUserData.nbLessons;
+    document.getElementById("nbReviews").innerHTML = wkUserData.nbReviews;
+    document.getElementById("reviewTime").innerHTML = wkUserData.nextReview;
+    document.getElementById("srsNbApprentice").innerHTML =
+      wkUserData.srsNbApprentice;
+    document.getElementById("srsNbGuru").innerHTML = wkUserData.srsNbGuru;
+    document.getElementById("srsNbMaster").innerHTML = wkUserData.srsNbMaster;
+    document.getElementById("srsNbEnlighten").innerHTML =
+      wkUserData.srsNbEnlighten;
+    document.getElementById("srsNbBurned").innerHTML = wkUserData.srsNbBurned;
 
-     document.getElementById('username').innerHTML = wkUserData.username;
-     document.getElementById('level').innerHTML = wkUserData.level;
-     document.getElementById('title').innerHTML = wkUserData.title;
-     document.getElementById('nbLessons').innerHTML = wkUserData.nbLessons;
-     document.getElementById('nbReviews').innerHTML = wkUserData.nbReviews;
-     document.getElementById('reviewTime').innerHTML = wkUserData.nextReview;
-     document.getElementById('srsNbApprentice').innerHTML = wkUserData.srsNbApprentice;
-     document.getElementById('srsNbGuru').innerHTML = wkUserData.srsNbGuru;
-     document.getElementById('srsNbMaster').innerHTML = wkUserData.srsNbMaster;
-     document.getElementById('srsNbEnlighten').innerHTML = wkUserData.srsNbEnlighten;
-     document.getElementById('srsNbBurned').innerHTML = wkUserData.srsNbBurned;
-
-     if (wkUserData.nbReviews > 0 || !wkUserData.nextReview){
-         // the user has reviews, or does not have next reviews
-         document.querySelector("#reviews").style.display = 'block';
-         document.querySelector("#nextReviews").style.display = 'none';
-     } else {
-         // the user does not have available reviews, display when will be the next one
-         document.querySelector("#reviews").style.display = 'none';
-         document.querySelector("#nextReviews").style.display = 'block';
-     }
+    if (wkUserData.nbReviews > 0 || !wkUserData.nextReview) {
+      // the user has reviews, or does not have next reviews
+      document.querySelector("#reviews").style.display = "block";
+      document.querySelector("#nextReviews").style.display = "none";
+    } else {
+      // the user does not have available reviews, display when will be the next one
+      document.querySelector("#reviews").style.display = "none";
+      document.querySelector("#nextReviews").style.display = "block";
+    }
   }
-}
+};
